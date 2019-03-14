2a3c21e6f69da7499538b4fcc423f7e487d12052
Adnan Azim <saazim1123@gmail.com>
2019-03-14 04:30:55 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,6 +15,7 @@ def first_challenge
 
   #your code here
   contacts["Freddy Mercury"][:favorite_icecream_flavors][0].shift
+  
 
   #remember to return your newly altered contacts hash!
   contacts

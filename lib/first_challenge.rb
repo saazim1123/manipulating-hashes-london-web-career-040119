1187bbd55071232ce0356e09218897698c78a12a
Adnan Azim <saazim1123@gmail.com>
2019-03-14 04:40:37 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,13 @@ def first_challenge
   }
 
   #your code here
- 
+  contacts.each do |person, data|
+    data.each do |attr, value|
+      if attr == :favorite_icecream_flavors
+        value.delete_if { |flavor| flavor == "strawberry" }
+      end
+    end
+  end
 
   #remember to return your newly altered contacts hash!
   contacts

eab298153c1538f462ddf804dd37801e8381c42e
Adnan Azim <saazim1123@gmail.com>
2019-03-14 04:43:20 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -17,7 +17,7 @@ def first_challenge
   contacts.each do |contact, data|
     data.each do |key, value|
       if key == :favorite_icecream_flavors
-        value.deleteif
+        value.delete_if do
       end
       
     end
